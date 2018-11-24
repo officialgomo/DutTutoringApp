ca5477d2d011f7ccc4cf322688e251dc8e53ca95
Gomolemo <officialgomo@gmail.com>
2018-11-24 11:06:26 +0200

    Add project files.

---
+++ DutTutoringApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DutTutoringApp.MvcApplication" Language="C#" %>
---
+++ DutTutoringApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DutTutoringApp.Startup))]
+namespace DutTutoringApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
